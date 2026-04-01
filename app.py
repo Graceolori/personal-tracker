0760521bd821fc315bbb994687fee05fe2ac1585
Graceolori <golori84@gmail.com>
2026-04-01 15:28:43 +0100

    Use localhost as default Flask bind address

--- app.py
+++ app.py
@@ -388,7 +388,7 @@ init_db()
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("FLASK_HOST", "0.0.0.0"),
+        host=os.getenv("FLASK_HOST", "127.0.0.1"),
         port=int(os.getenv("FLASK_PORT", "5000")),
         debug=os.getenv("FLASK_DEBUG", "0") == "1",
     )
